5b1426b4dad45473cc1790f5c00639b2a19bf136
gquesnot <gaqu1994@gmail.com>
2021-11-23 20:30:01 +0100

    dd

--- js/food.js
+++ js/food.js
@@ -3,7 +3,6 @@ export class Food{
         this.name = name
         this.htmlTag = document.createElement("li");
         this.htmlTag.innerText = name
-        console.log(htmlFoodList)
         htmlFoodList.appendChild(this.htmlTag);
     }
 
